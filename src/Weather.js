d95a47eaf000debcd7b1f7a3f169a09b0466d544
Branden Ge <86636108+brandenge@users.noreply.github.com>
2022-08-25 14:42:49 -0500

    Weather and Movies APIs are working and talking to front end

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Weather extends React.Component {
   render() {
-    const weather = this.props.days.map(day => <li>Date: {day.date} | Weather: {day.description}</li>);
+    const weather = this.props.days.map((day, i) => <li key={i}>Date: {day.date} | Weather: {day.description}</li>);
     return (
       <ol>
         {weather}
